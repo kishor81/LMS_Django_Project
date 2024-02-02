09c93aa1158ee5d55d81d5688168522b5a57d65f
Kishor M <kishwormagranti.11@gmail.com>
2024-02-02 17:58:18 +0545

    second version

--- lms/urls.py
+++ lms/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
+     path('', lambda request: redirect('admin/', permanent=False)),
     path('admin/', admin.site.urls),
     path('api/', include('library.urls')),
     path('api/users/', include('library.urls')),
